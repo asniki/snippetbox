8c14041005779238e71bdf94bab46566c45aaf1e
asniki12 <asniki12@mts.ru>
2025-07-07 08:52:44 +0300

    Add TestSnippetCreate

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -192,3 +192,36 @@ func TestUserSignup(t *testing.T) {
 		})
 	}
 }
+
+func TestSnippetCreate(t *testing.T) {
+	app := newTestApplication(t)
+	ts := newTestServer(t, app.routes())
+	defer ts.Close()
+
+	t.Run("Unauthenticated", func(t *testing.T) {
+		code, headers, _ := ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusSeeOther)
+		assert.Equal(t, headers.Get("Location"), "/user/login")
+	})
+
+	t.Run("Authenticated", func(t *testing.T) {
+		_, _, body := ts.get(t, "/user/login")
+
+		validCSRFToken := extractCSRFToken(t, body)
+
+		form := url.Values{}
+		form.Add("email", "bob@example.com")
+		form.Add("password", "validPa$$word")
+		form.Add("csrf_token", validCSRFToken)
+
+		code, _, _ := ts.postForm(t, "/user/login", form)
+
+		assert.Equal(t, code, http.StatusSeeOther)
+
+		code, _, body = ts.get(t, "/snippet/create")
+
+		assert.Equal(t, code, http.StatusOK)
+		assert.StringContains(t, body, "<form action='/snippet/create' method='POST'>")
+	})
+}
